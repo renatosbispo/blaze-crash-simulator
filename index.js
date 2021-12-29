86a646b3a3d0b4a98e5ed4c0062ec27d51cc2744
Renato Bispo <renatoslvbispo@gmail.com>
2021-12-28 22:10:44 -0300

    Setup program data directory if it does not exist

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 #!/usr/bin/env node
 
+import fs from 'fs';
+import { PROGRAM_DATA_DIRECTORY_PATH } from './lib/filesystem.js';
+import printError from './lib/error-handling.js';
+
+
 function start() {
-  console.log('Running...');
+  if (!fs.existsSync(PROGRAM_DATA_DIRECTORY_PATH)) {
+    console.log('Program data directory not found.');
+    console.log(`Creating it at ${PROGRAM_DATA_DIRECTORY_PATH}...`);
+
+    try {
+      fs.mkdirSync(PROGRAM_DATA_DIRECTORY_PATH, 'utf8');
+      console.log('Successfully created program data directory.');
+    } catch (error) {
+      printError(error);
+
+      return;
+    }
+  }
 }
 
 start();
