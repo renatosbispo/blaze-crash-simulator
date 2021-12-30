892d117d8263955076f16cc64096c5fcb08bd797
Renato Bispo <renatoslvbispo@gmail.com>
2021-12-30 18:27:09 -0300

    Rename variables to match previous refactoring

--- index.js
+++ index.js
@@ -6,9 +6,8 @@ import {
   createProgramDataDirectory,
 } from './lib/filesystem.js';
 import printError from './lib/error-handling.js';
-import collectCrashPointsHistorySample from './lib/crash-points-history-sample-collection.js';
 
-function start() {
+async function start() {
   if (!programDataDirectoryExists()) {
     console.log('Program data directory not found.');
     console.log(`Creating it at ${PROGRAM_DATA_DIRECTORY_PATH}...`);
