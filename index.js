9499f5d4c6366db2f9c1c23f3195e59e6c49f172
Renato Bispo <renatoslvbispo@gmail.com>
2022-01-02 23:32:29 -0300

    Fully implement crash points sample creation

--- index.js
+++ index.js
@@ -2,14 +2,18 @@
 
 import {
   PROGRAM_DATA_DIRECTORY_PATH,
+  CRASH_POINTS_HISTORY_SAMPLES_FILE_PATH,
   programDataDirectoryExists,
   createProgramDataDirectory,
+  programDataExists,
+  saveProgramData,
 } from './lib/filesystem.js';
 import printError from './lib/error-handling.js';
+import createCrashPointsHistorySample from './lib/crash-points-history-sample-collection.js';
 
 async function start() {
   const printSeparator = () => {
-    console.log('---------------------------------------------------');
+    console.log('\n\r---------------------------------------------------\n\r');
   };
 
   if (!programDataDirectoryExists()) {
@@ -19,7 +23,32 @@ async function start() {
     try {
       createProgramDataDirectory();
 
-      console.log('> [BCSim] Program data directory successfully created.');
+      console.log('> [BCSim] Program data directory was successfully created.');
+      printSeparator();
+    } catch (error) {
+      printError(error);
+
+      return;
+    }
+  }
+
+  if (!programDataExists(CRASH_POINTS_HISTORY_SAMPLES_FILE_PATH)) {
+    console.log('> [BCSim] No crash points history samples were found.');
+    console.log('> [BCSim] Creating new sample...');
+
+    try {
+      const newCrashPointsHistorySample =
+        await createCrashPointsHistorySample();
+
+      console.log('> [BCSim] Sample was successfully created.');
+      console.log('> [BCSim] Saving sample to program data directory...');
+
+      saveProgramData(
+        [newCrashPointsHistorySample],
+        CRASH_POINTS_HISTORY_SAMPLES_FILE_PATH
+      );
+
+      console.log('> [BCSim] Sample was successfully saved.');
       printSeparator();
     } catch (error) {
       printError(error);
